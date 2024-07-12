defc89cc879e9d6f64207d882931a2f9d0cb2539
Amo Mpete <amompete@gmail.com>
2024-07-12 09:45:02 -0700

    Processes and signals

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,41 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// Function prototype
+/* Function prototype */
 int infinite_while(void);
 
 int main(void)
 {
-    pid_t pid;
-    int i;
+	pid_t pid;
+	int i;
 
-    for (i = 0; i < 5; i++) {
-        pid = fork();
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
 
-        if (pid < 0) {
-            perror("Fork failed");
-            return 1;
-        } else if (pid == 0) {
-            // Child process
-            printf("Zombie process created, PID: %d\n", getpid());
-            exit(0); // Child exits immediately, becoming a zombie
-        } else {
-            // Parent process
-            sleep(1); // Let the child finish before spawning the next one
-        }
-    }
+		if (pid < 0)
+		{
+			perror("Fork failed");
+			return (1);
+		}
+		else if (pid == 0)
+		{
+			/* Child process */
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0); /* Child exits immediately, becoming a zombie */
+		}
+		else
+		{
+			/* Parent process */
+			sleep(1); /* Let the child finish before spawning the next one */
+		}
+	}
 
-    // Parent process goes into an infinite loop
-    infinite_while();
+	/* Parent process goes into an infinite loop */
+	infinite_while();
 
-    return 0;
+	return (0);
 }
 
-// Infinite loop function
+/* Infinite loop function */
 int infinite_while(void)
 {
-    while (1) {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
